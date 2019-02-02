104990baa8957deab3a8add1b25af35628cc8386
Park Giyong <gemfile0@gmail.com>
2019-02-03 01:01:43 +0900

    Initial Commit

---
+++ ReverseString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using Xunit;
+
+namespace DotnetTests
+{
+    public class ReverseString
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("dcba", Reverse("abcd"));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal("dcba  ", Reverse("  abcd"));
+        }
+
+        string Reverse(string str)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char c in str)
+            {
+                result.Insert(0, c);
+            }
+
+            return result.ToString();
+        }
+    }
+}
